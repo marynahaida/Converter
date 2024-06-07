4c671ad93a43923c7e3dca73806f4d0a73182ebe
marynahaida <marinkagaida.15@gmail.com>
2024-06-07 11:52:09 +0200

    Add convertation from miles to km

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,35 @@
 package app;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Converter from miles to km and from km to miles");
+
+        int z = 10;
+        while (z > 1) {
+            System.out.println("Select what do you want to do\n" +
+                    "\t1. Convert miles to km\n" +
+                    "\t0. Exit");
+            int q = scanner.nextInt();
+            switch (q){
+                case 0: z = 0; break;
+                case 1:
+                    System.out.print("Miles: ");
+                    double m = scanner.nextDouble();
+                    System.out.println("Km: " + milesToKm(m));
+                    break;
+                default:
+                    System.out.println("You did entered wrong value");
+                    break;
+            }
+        }
     }
+    public static double milesToKm (double miles) {
+        double km = miles * 1.60934;
+        return km;
+    }
+
 }
